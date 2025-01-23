bc34ffe7dc113caf75a4379dcaf1212e00ed085a
abdullah107189 <abdullah107189@gmail.com>
2025-01-23 11:52:00 +0600

    create profile page and header section

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import Link from "next/link";
+// import NavbarPage from "@/component/navbar/page";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -23,7 +25,39 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        {/* <>
+          <NavbarPage></NavbarPage>
+        </> */}
+        <div className="bg-white">
+          <div className="flex py-2 items-center justify-between sticky top-0 z-10 max-w-[1440px] mx-auto px-2">
+            <>
+              <a className="text-2xl font-bold">
+                <span className="text-blue-400">B</span>P
+              </a>
+            </>
+            <div className="flex gap-5">
+              <Link
+                className="bg-blue-400/20 text-blue-400 font-semibold px-4 py-1 rounded-lg"
+                href={"/"}
+              >
+                Home
+              </Link>
+              <Link
+                className="bg-blue-400/20 text-blue-400 font-semibold px-4 py-1 rounded-lg"
+                href={"/profile"}
+              >
+                Profile
+              </Link>
+            </div>
+            <>
+              <button className="bg-blue-400/20 text-blue-400 font-semibold px-4 py-1 rounded-lg">
+                Login
+              </button>
+            </>
+          </div>
+        </div>
+
+        <main className="max-w-[1440px] mx-auto px-2">{children}</main>
       </body>
     </html>
   );
---
+++ src/app/profile/page.js
@@ -0,0 +1,14 @@
+const ProfilePage = () => {
+  return (
+    <div className="bg-white md:p-10 p-5 md:my-10 my-5 rounded-3xl">
+      <h1 className="md:text-4xl text-xl  font-semibold text-center md:my-10 my-5">
+        hello , abdullah
+      </h1>
+      <p className="md:text-5xl text-2xl font-semibold text-center">
+        Well to your profile!!
+      </p>
+    </div>
+  );
+};
+
+export default ProfilePage;
