c9738d8379a2db958cb8ec2d55ae3c8cc4bde710
abdullah107189 <abdullah107189@gmail.com>
2025-01-23 17:08:28 +0600

    added full user name in the profile page

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -5,14 +5,14 @@ const ProfilePage = async () => {
   const { getUser, isAuthenticated } = getKindeServerSession();
   const user = await getUser();
   const isUserAuthenticated = await isAuthenticated();
+  console.log("user...............", user);
   if (!isUserAuthenticated) {
-    console.log("its hits...........");
     redirect("/api/auth/login");
   }
   return (
     <div className="bg-white md:p-10 p-5 md:my-10 my-5 rounded-3xl">
       <h1 className="md:text-4xl text-xl  font-semibold text-center md:my-10 my-5">
-        hello {user?.family_name}
+        Hello, {user?.given_name} {user?.family_name}
       </h1>
       <p className="md:text-5xl text-2xl font-semibold text-center">
         Welcome to your profile!!
